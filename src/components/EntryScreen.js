168ae76c4cfa0b3426171731278bde64933f0cbc
raulriato <raulriato@gmail.com>
2022-07-24 21:21:10 -0300

    feat finished entry screen layout

--- src/components/EntryScreen.js
+++ src/components/EntryScreen.js
@@ -1,8 +1,9 @@
 export default function EntryScreen(){
     return (
-        <>
+        <div class="entry-screen">
             <img src="./images/entryzap.png" alt='' />
             <h1>ZapRecall</h1>
-        </>
+            <button>Iniciar Recall!</button>
+        </div>
     )
 }
